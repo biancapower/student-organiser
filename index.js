85ff492af697c39e2b21714e0d3249d5eaad2742
Bianca Power <drbiancampower@gmail.com>
2018-06-09 19:49:07 +1000

    add function to shuffle list of students

---
+++ index.js
@@ -0,0 +1,19 @@
+// names of all students
+const students = ["Alex", "Andres", "Andrew", "Ash", "Casey", "Cassandra", "Charles", "Chris B", "Chris F", "Chris L", "Dae", "Elouise", "Frank", "Hadi", "Jake", "James", "Jen", "Joseph", "Katie", "Lewis", "Luke", "Lux", "Maryna", "Mat", "Nick", "Punya", "Rafael", "Rhys", "Sam F", "Sam R"];
+
+// number of students per group
+const groupSize = 3;
+
+// shuffle names of students
+const shuffledStudents = shuffle(students);
+
+// function to shuffle an array
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+
